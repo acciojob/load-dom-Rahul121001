228d8d4f750a11b710a6f9af6e53977465aafb78
Rahul121001 <139845999+Rahul121001@users.noreply.github.com>
2023-09-24 20:34:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+  // Write the string "DOM load success" on the web page
+  document.body.innerHTML += "<p>DOM load success</p>";
+});
+
